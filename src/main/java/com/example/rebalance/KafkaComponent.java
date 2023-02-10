fd3946343b675dbbb98a4fbcdaa78ebd10f68466
Viacheslav Chernyshev <v.chernyshev@outlook.com>
2023-02-10 18:12:29 +0000

    Remove unused DltHandler method

--- src/main/java/com/example/rebalance/KafkaComponent.java
+++ src/main/java/com/example/rebalance/KafkaComponent.java
@@ -3,7 +3,6 @@ package com.example.rebalance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.TransientDataAccessResourceException;
-import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
@@ -17,10 +16,5 @@ public class KafkaComponent {
     public String processMessage(String message) {
         logger.info("Processing message=\"{}\"", message);
         throw new TransientDataAccessResourceException("Simulating a transient failure");
-    }
-
-    @DltHandler
-    public void processDeadLetterMessage(String message) {
-        logger.error("Failed to process message=\"{}\"", message);
     }
 }
